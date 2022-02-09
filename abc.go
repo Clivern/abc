f45a66e1f21c8400e3ada7b552e57d8ffba5b959
Clivern <hello@clivern.com>
2022-02-09 10:02:10 +0100

    test docker release

---
+++ abc.go
@@ -0,0 +1,13 @@
+// Copyright 2021 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World")
+}
